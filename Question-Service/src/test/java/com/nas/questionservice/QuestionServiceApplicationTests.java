173c3013646b59b2d5d22dba4f6186ebdf076d69
info <nasihz12345@gmail.com>
2023-12-06 21:19:15 -0500

    "first micro serice added"

---
+++ Question-Service/src/test/java/com/nas/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nas.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
