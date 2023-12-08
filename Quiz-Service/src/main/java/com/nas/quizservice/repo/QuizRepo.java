770dccdee5700bea58308a454aa2c3649082fea3
info <nasihz12345@gmail.com>
2023-12-08 15:51:27 -0500

    aa

--- Quiz-Service/src/main/java/com/nas/quizservice/repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nas.quizservice.repo;
-
-import com.nas.quizservice.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz, Integer> {
-}
