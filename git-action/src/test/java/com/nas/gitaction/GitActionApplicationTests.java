0268a5723a79616cdebcf01d625e0b111235ac34
info <nasihz12345@gmail.com>
2023-12-08 15:00:03 -0500

    "new projects"

---
+++ git-action/src/test/java/com/nas/gitaction/GitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nas.gitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
